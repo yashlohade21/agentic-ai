26f0ef1ae020509aba79a14e80e592e5de9a6624
yashlohade21 <yashlohade125@gmail.com>
2025-07-16 12:27:50 +0530

    made the things more good and added one more api yeahhhh

--- backend/app.py
+++ backend/app.py
@@ -19,7 +19,9 @@ def create_app():
     'http://localhost:3000', 
     'http://127.0.0.1:3000',
     'http://localhost:5173',
-    'http://127.0.0.1:5173'
+    'http://127.0.0.1:5173',
+    'https://ai-agent-with-frontend.onrender.com/',
+    'https://ai-agent-zeta-bice.vercel.app/'
     ])
     
     # Register blueprints
