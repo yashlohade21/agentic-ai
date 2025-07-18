e8129976a333cc70b364a7ec87124451d0e41330
yashlohade21 <yashlohade125@gmail.com>
2025-07-18 11:30:24 +0530

    Firebase Integration Problem Solved Try 5

--- backend/firebase_config.py
+++ backend/firebase_config.py
@@ -1,10 +1,13 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
+import os
 
 # Initialize Firebase Admin SDK
-cred = credentials.Certificate("./google-services.json")
-firebase_admin.initialize_app(cred)
+FIREBASE_SECRET_PATH = os.getenv("FIREBASE_SECRET_PATH")
 
-db = firestore.client()
-
-
+if FIREBASE_SECRET_PATH:
+    cred = credentials.Certificate(FIREBASE_SECRET_PATH)
+    firebase_admin.initialize_app(cred)
+    db = firestore.client()
+else:
+    raise Exception("FIREBASE_SECRET_PATH environment variable not set.")
